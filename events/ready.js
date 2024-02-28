adc6c93a24a0e18fab8da3f1edef622125249023
qFlux <140560637+qFlox@users.noreply.github.com>
2024-02-28 12:40:45 +1000

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,47 @@
+// index.js
+const { ActivityType, Events } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const activities = require("../config").activities;
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute: async (client) => {
+    const rest = new REST({ version: "10" }).setToken(client.token);
+    let index = 0;
+
+    const updateActivity = () => {
+      client.user.presence.set({
+        activities: [{ name: activities.data[index], type: ActivityType.Listening }],
+      });
+      index = (index + 1) % activities.data.length;
+    };
+
+    updateActivity();
+    setInterval(updateActivity, 1000);
+
+    console.log(' ');
+    console.info(' |       _                   _          ');
+    console.info(' |      |_) ._ o  _  |_ _|_ |_)  _      ');
+    console.info(' |      |_) |  | (_| | | |_ |_) (_)     ');
+    console.info(' |                _|                    ');
+    console.info(' |   ');
+    console.info(' |      Made by FireKrill and qFlux_');
+    console.info(' |      License: GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007.');
+    console.log(' ');
+    console.log(' ');
+    console.log(`[✓] Bot online`);
+    console.log(`[✓] Bot online`);
+
+    try {
+      await rest.put(Routes.applicationCommands(client.user.id), {
+        body: client.slashDatas,
+      });
+      console.log('[✓] Slash commands successfully registered');
+    } catch (error) {
+      console.log(`[X] Error while registering slash commands`);
+      console.error(error);
+    }
+  },
+};
