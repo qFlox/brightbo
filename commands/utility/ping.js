e986b9153250b1dfab311efeadaaf65d93934c13
qFlux <140560637+qFlox@users.noreply.github.com>
2024-03-04 09:19:14 +1000

    Update ping.js

---
+++ commands/utility/ping.js
@@ -0,0 +1,32 @@
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const prefix = {
+  name: "ping",
+  cooldown: 5000,
+  ownerOnly: false,
+  run: async (client, message, args) => {
+    const embed = new EmbedBuilder()
+      .setTitle("Pong!")
+      .setDescription(`Ping: ${client.ws.ping}ms`)
+      .setColor("#5B21B6");
+    message.reply({ embeds: [embed] });
+  }
+};
+
+const slash = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Pong!"),
+  cooldown: 5000,
+  ownerOnly: false,
+  run: async (client, interaction) => {
+    const embed = new EmbedBuilder()
+      .setTitle("Pong!")
+      .setDescription(`Ping: ${client.ws.ping}ms`)
+      .setColor("#5B21B6");
+    interaction.reply({ embeds: [embed] });
+  }
+};
+
+module.exports = {slash, prefix};
